2eba6748c150c60388dcace9056e46dfcbb400e4
HARSHA K S <harshaksrinivas@gmail.com>
2022-01-05 11:10:10 +0530

    Update SampleJava.java

--- SampleJava.java
+++ SampleJava.java
@@ -1,7 +1,7 @@
 public class SampleJava{
 
 public static void main(String[] args){
- System.out.println("Sample java code!!!!");
+ System.out.println("Sample java code!!");
  System.out.println("Updated thrice");
 }
 }
